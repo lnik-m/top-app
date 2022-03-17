71b20b9385dce3751f3fca2d07ea2b52bd2c2175
lnik <marialnikova@gmail.com>
2022-03-17 19:44:43 +0300

    Создание стрелки для компоненты Button, настройка окружения для работы с svg файлами.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home(): JSX.Element {
       <Htag tag='h2'>привет2</Htag>
       <Htag tag='h3'>привет3</Htag>
       <Button appearance='primary' className='hello'>Узнать подробнее</Button>
-      <Button appearance='ghost'>Читать отзывы</Button>
+      <Button appearance='ghost' arrow='right'>Читать отзывы</Button>
     </div>
   );
 }
