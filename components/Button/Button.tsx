b0ed816851d716dc837faceccffdb8289b0027b9
lnik <marialnikova@gmail.com>
2022-03-17 18:05:21 +0300

    Компонента Buttton.

---
+++ components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import { ButtonProps } from './Button.props';
+import styles from './Button.module.scss';
+import cn from 'classnames';
+
+export const Button = ({ appearance, children, className, ...props }: ButtonProps): JSX.Element => {
+	return(
+		<button
+			className={cn(styles.button, className, {
+				[styles.primary]: appearance=='primary',
+				[styles.ghost]: appearance=='ghost'
+			}
+				)}
+				{...props}
+		>
+			{children}
+		</button>
+	);
+};
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag } from '../components';
+import { Htag, Button } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,8 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>привет1</Htag>
       <Htag tag='h2'>привет2</Htag>
       <Htag tag='h3'>привет3</Htag>
+      <Button appearance='primary' className='hello'>Узнать подробнее</Button>
+      <Button appearance='ghost'>Читать отзывы</Button>
     </div>
   );
 }
