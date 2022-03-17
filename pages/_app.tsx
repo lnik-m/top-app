38793bb749f0a36a1e07f6df9729baa196c9c7d0
lnik <marialnikova@gmail.com>
2022-03-17 14:05:44 +0300

    Подключение шрифта Noto Sans, вынос цветов в массив переменных $colors в styles/global.scss.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.scss';
 import {AppProps} from "next/app";
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps):JSX.Element {
-  return <Component {...pageProps} />;
+  return (<>
+    <Head>
+      <title>My top-app</title>
+    </Head>
+    <Component {...pageProps}/>
+  </>
+  );
 }
 
 export default MyApp;
