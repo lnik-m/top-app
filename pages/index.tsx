060f1ed62d924c5e77c7825db389930fc0a1dd41
lnik <marialnikova@gmail.com>
2022-03-17 20:47:43 +0300

    Компонент Tag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button, Paragraph } from '../components';
+import { Htag, Button, Paragraph, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -6,12 +6,21 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>Heading 1</Htag>
       <Htag tag='h2'>Heading 2</Htag>
       <Htag tag='h3'>Heading 3</Htag>
+
       <Button appearance='primary' arrow='down' className='hello'>Узнать подробнее</Button>
       <Button appearance='ghost' arrow='down'>Читать отзывы</Button>
       <Button appearance='ghost' arrow='right'>Читать отзывы</Button>
+
       <Paragraph size='s'>small</Paragraph>
       <Paragraph size='m'>medium</Paragraph>
       <Paragraph size='b'>big</Paragraph>
+
+      <Tag size='m' color='l-blue'>10</Tag>
+      <Tag size='s' color='ghost'>Photoshop</Tag>
+      <Tag size='m' color='red'>hh.ru</Tag>
+      <Tag size='s' color='primary'>Подготовка макетов</Tag>
+      <Tag size='m' color='green'>-10 000 ₽ </Tag>
+      
     </div>
   );
 }
