e0cb31bbe293fa763433ca4d4b151071a7488e77
lnik <marialnikova@gmail.com>
2022-03-25 12:05:31 +0300

    Верстка sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,11 +2,18 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.scss';
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
+import LogoIcon from '../logo.svg';
+import SearchIcon from './search.svg';
 
 export const Sidebar = ( {className, ...props }: SidebarProps): JSX.Element => {
 
 	return(
 		<div className={cn(className, styles.sidebar)} {...props}>
+			<LogoIcon />
+			<div className={styles.search}>
+				<span>Поиск...</span>
+				<div className={styles.icon}><SearchIcon /></div>
+			</div>
 			<Menu />
 		</div>
 	);
